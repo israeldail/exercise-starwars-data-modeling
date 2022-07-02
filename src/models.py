b67c716aadd7726bb17f79dc9b48cd51fd91259c
israeldail <israeldail2@gmail.com>
2022-07-02 00:40:34 +0000

    done

--- src/models.py
+++ src/models.py
@@ -13,6 +13,24 @@ class UserLogin(Base):
     user_name = Column(String(250), nullable=False)
     password = Column(String(250), nullable=False)
 
+class Characters(Base):
+    __tablename__ = "characters"
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String(250))
+    homeworld = Column(String(250))
+    gender = Column(String(250))
+    height = Column(String(250))
+    mass = Column(Integer, ForeignKey('mass'))
+    eye_color = Column(String(250))
+
+class Planets(Base):
+    __tablename__ = "planets"
+    id = Column(Integer, primary_key=True)
+    planet_name = Column(String(250))
+    planet_mass = Column(Integer, ForeignKey('planet_mass'))
+    planet_coordinates = Column(Integer, ForeignKey('planet_coordinates'))
+
+
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
@@ -22,6 +40,11 @@ class User(Base):
     email_address = Column(String(250), nullable=False)
     userlogin_id = Column(Integer, ForeignKey(UserLogin.id))
     userlogin = relationship(UserLogin)
+    planets_column = Column(Integer, ForeignKey('planets.id'))
+    planets_id = relationship(Planets)
+    characters_column = Column(Integer, ForeignKey('characters.id'))
+    characters_id = relationship(Characters)
+
 
 # class Person(Base):
 #     __tablename__ = 'person'
@@ -41,39 +64,28 @@ class User(Base):
 #     person_id = Column(Integer, ForeignKey('person.id'))
 #     person = relationship(Person)
 
-class Characters(Base):
-    __tablename__ = "characters"
-    id = Column(Integer, primary_key=True)
-    character_name = Column(String(250))
-    homeworld = Column(String(250))
-    gender = Column(String(250))
-    height = Column(String(250))
-    mass = Column(Integer, ForeignKey('mass'))
-    eye_color = Column(String(250))
 
 
-class Planets(Base):
-    __tablename__ = "planets"
-    id = Column(Integer, primary_key=True)
-    planet_name = Column(String(250))
-    planet_mass = Column(Integer, ForeignKey('planet_mass'))
-    planet_coordinates = Column(Integer, ForeignKey('planet_coordinates'))
+
+
 
 class FavoritePlanet(Base):
     __tablename__ = "favorite_planets"
     id = Column(Integer, primary_key=True)
     user_favorite_planet = Column(String(250), ForeignKey('planets.id'))
     planet_id = relationship(Planets)
-    user_profile = Column(Integer, ForeignKey('user.id'))
-    user_id = relationship(User)
+   
 
 class FavoriteChar(Base):
     __tablename__ = "favorite_characters"
     id = Column(Integer, primary_key=True)
     user_favorite_char = Column(String(250), ForeignKey('characters.id'))
     characters_id = relationship(Characters)
-    user_profile = Column(Integer, ForeignKey('user.id'))
-    user_id = relationship(User)
+    
+
+
+
+
 
 
     def to_dict(self):
